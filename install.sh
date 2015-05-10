8129a4cf7053a623ca1e8c3472172f749363c60d
Shaps Mohsenin <shaps@theappbusiness.com>
2015-05-10 19:23:36 +0100

    Updated script

--- install.sh
+++ install.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
 echo 'Downloading script.sh ...'
-#curl -o /tmp/script.sh https://raw.githubusercontent.com/shaps80/bash/master/script.sh
-
-echo 'Backing up to ~/script_backup ...'
+curl -o /tmp/script.sh https://raw.githubusercontent.com/shaps80/bash/master/script.sh
 
 bashrc=".bashrc"
 bashprofile=".bash_profile"
 backup="~/script_backup"
+script="script.sh"
 
 if [ -z "$backup" ]; then
   mkdir "$backup"
 fi
 
 if  [ ! -z "~/$bashrc" ]; then
-  echo "copying " "~/$bashrc" "to" "$backup/$bashrc"
-#  cp "~/$bashrc" "$backup/$bashrc"
+  echo ""
+  echo "Backing up ~/$bashrc to $backup/$bashrc"
+  cp "~/$bashrc" "$backup/$bashrc"
 fi
 
 if  [ ! -z "~/$bashprofile" ]; then
-  echo "Copying " "~/$bashprofile" "to" "$backup/$bashprofile"
-#  cp "~/$bashprofile" $backup/$bashprofile
+  echo "Backing up ~/$bashprofile to $backup/$bashprofile"
+  cp "~/$bashprofile" $backup/$bashprofile
 fi
 
-echo "Would you like to save this as (1) ~/.bashrc or (2) ~/.bash_profile -- defaults to 1"
-read -n result
+echo ""
+printf "\t 1: ~/.bash_profile (default)\n"
+printf "\t 2: ~/.bashrc\n\n"
+printf "Where would you like to install this script: "
 
-if [ $result == 1]; then
-  
-fi
+read -n 1 result
+echo ""
 
+case ${answer:0:1} in
+    2) 
+    echo "Copying $script to ~/$bashrc"
+    cp $script ~/$bashrc
+    ;;
+    *) 
+    echo "Copying ~/$script to ~/$bashprofile" 
+    cp $script ~/$bashprofile
+    
+    ;;
+esac
+
+echo ""
 echo 'Done - restart or logout to load this script'
